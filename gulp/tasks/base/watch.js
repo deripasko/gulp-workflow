2ffcf2ecdb0f27d66958d383f6835a3cf5c76f36
--replace-all <mandooox@gmail.com>
2018-03-20 14:47:56 +0200

    remove vendor and add js watch task from watch tasks

--- gulp/tasks/base/watch.js
+++ gulp/tasks/base/watch.js
@@ -3,7 +3,6 @@
 
 // ----------------------------------
 // watch tasks: 
-//    bower
 //    sass
 //    nunjucks
 //    js
@@ -12,8 +11,8 @@
 module.exports = function(gulp, $, path, config) {
 
     gulp.task('watch', 'watch files when changes', function() {
-    	// gulp.watch(config.mainYarnFiles.watch, [config.task.vendor]);
         gulp.watch(path.to.sass.src, [config.task.sass]);
+        gulp.watch(path.to.js.watch, [config.task.scripts]);
         gulp.watch(path.to.nunjucks.watch, [config.task.nunjucks]);
     });
 
